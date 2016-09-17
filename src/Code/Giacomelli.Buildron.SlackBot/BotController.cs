6a54021a2ce371c03fb8182c3db38e9035d3f1d5
Diego Giacomelli <giacomelli@gmail.com>
2016-09-17 14:23:51 -0300

    First version sending BuildStatusChanged using WWWForm.

---
+++ src/Code/Giacomelli.Buildron.SlackBot/BotController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using Buildron.Domain.Mods;
+using Skahal.Logging;
+using Skahal.Threading;
+using UnityEngine;
+
+namespace Giacomelli.Buildron.SlackBot
+{
+	public class BotController : MonoBehaviour
+	{
+		#region Fields
+		private IModContext m_modContext;
+		private string m_slackKey;
+		private ISHLogStrategy m_log;
+		#endregion
+
+		#region Methods
+		private void Start()
+		{
+			m_modContext = Mod.Context;
+			m_log = m_modContext.Log;
+		
+			m_slackKey = m_modContext.Preferences.GetValue<string>("Token");
+
+			if (String.IsNullOrEmpty(m_slackKey))
+			{
+				m_log.Warning("Will not start, because Token is empty.");
+				return;
+			}
+
+			m_modContext.BuildStatusChanged += (sender, e) =>
+			{
+				var b = e.Build;
+				Send("{0} - {1}: {2}", b.Configuration.Project.Name, b.Configuration.Name, b.Status.ToString().ToLowerInvariant());
+			};
+		}
+
+		/// <summary>
+		/// Send the specified message and args.
+		/// https://api.slack.com/methods/chat.postMessage
+		/// </summary>
+		/// <param name="message">Message.</param>
+		/// <param name="args">Arguments.</param>
+		private void Send(string message, params object[] args)
+		{
+			var formattedMsg = message.With(args);
+			m_log.Debug("Sending message '{0}'...", formattedMsg);
+			var url = "https://slack.com/api/chat.postMessage";
+			var data = new WWWForm();
+			data.AddField("token", m_slackKey);
+			data.AddField("channel", "general");
+			data.AddField("text", formattedMsg);
+			data.AddField("username", "Buildron");
+			data.AddField("icon_url", "https://raw.githubusercontent.com/skahal/Buildron/master/docs/images/Buildron-logo-128x128.png");
+			var post = new WWW(url, data);
+
+			StartCoroutine(WaitForPost(post));
+		}
+
+		private IEnumerator WaitForPost(WWW post)
+		{
+			yield return post;
+
+			m_log.Debug("Message sent.");
+		}
+		#endregion
+	}
+}
--- src/Code/Giacomelli.Buildron.SlackBot/Mod.cs
+++ src/Code/Giacomelli.Buildron.SlackBot/Mod.cs
@@ -4,13 +4,18 @@ namespace Giacomelli.Buildron.SlackBot
 {
     public class Mod : IMod
     {
+		public static IModContext Context { get; private set; }
+
         public void Initialize(IModContext context)
         {
-			context.BuildStatusChanged += (sender, e) =>
+			Context = context;
+
+			//xoxb-80789644979-uQMlJbghIvWZkXVQJP9eQrWg
+			context.Preferences.Register(new Preference("Token", "Slack key", PreferenceKind.String, string.Empty));
+
+			context.CIServerConnected += (sender, e) =>
 			{
-				var box = context.CreateGameObjectFromPrefab("BoxPrefab");
-				var controller = box.AddComponent<BoxController>();
-				controller.SetModel(e.Build);
+				context.GameObjects.Create<BotController>();
 			};
         }
     }
